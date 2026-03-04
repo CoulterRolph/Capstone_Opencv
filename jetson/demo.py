129982f0d0036ece7cc09232ed74e1e8a405bf1a
Deeplowmah <134818299+Deeplowmah@users.noreply.github.com>
2026-03-03 23:19:24 -0500

    trained new model for yolo26n-pose

---
+++ jetson/demo.py
@@ -0,0 +1,3 @@
+## Run the model using hardware acceleration on Jetson
+from ultralytics import YOLO
+import cv2 as cv
--- train_table/train.py
+++ train_table/train.py
@@ -4,5 +4,25 @@ import cv2 as cv
 
 print(cv.__version__)
 
-model = YOLO("yolov8n-pose.pt")
-model.train(data="C:\\Users\\diplo\\Desktop\\Capstone\\OpenCV\\train_table\\data.yaml", epochs=100, imgsz=640, batch=16)
+## model = YOLO("yolo26n-pose.pt")
+## model.train(data="C:\\Users\\melon\\OneDrive\\Desktop\\OpenCV\\train_table\\data.yaml", epochs=100, imgsz=640, batch=32)
+
+
+model = YOLO("yolo26n-pose.pt")  # or whatever pose checkpoint you're using
+
+model.train(
+    data="C:\\Users\\melon\\OneDrive\\Desktop\\OpenCV\\train_table\\data.yaml",
+    epochs=300,
+    patience=20,          # early stopping
+    imgsz=640,
+    batch=-1,             # autobatch
+    ## device=0,             # GPU 0 (change if needed)
+    workers=2,            # Windows-safe start; raise if stable
+    cache=True,
+    amp=True,
+    optimizer="auto",
+    cos_lr=True,
+    close_mosaic=10,
+    project="train_table",
+    name="y26n_pose_640"
+)
