411e7395aba6eb95281b775b9f5026aa04a46752
Deeplowmah <134818299+Deeplowmah@users.noreply.github.com>
2026-03-23 08:19:45 -0400

    trained ball_player_best_001.pt

--- train_ball_player/train.py
+++ train_ball_player/train.py
@@ -1,8 +1,23 @@
-# training python code for ping pong bot
 from ultralytics import YOLO
 import cv2 as cv
 
-print(cv.__version__)
+model = YOLO("yolo26n.pt")  # or whatever pose checkpoint you're using
 
-model = YOLO("yolov8n-pose.pt")
-model.train(data="C:\\Users\\diplo\\Desktop\\Capstone\\OpenCV\\train_table\\data.yaml", epochs=100, imgsz=640, batch=16)
+model.train(
+    data="C:\\Users\\melon\\OneDrive\\Desktop\\OpenCV\\train_ball_player\\data.yaml",
+    epochs=300,
+    save=True,
+    save_period=20,   # save a checkpoint every 10 epochs
+    patience=75,         # early stopping
+    imgsz=640,
+    batch=30,             # -1 autobatch
+    ## device=0,          # GPU 0 (change if needed)
+    workers=4,            # Windows-safe start; raise if stable
+    cache=True,
+    amp=True,
+    optimizer="auto",
+    cos_lr=True,
+    close_mosaic=10,
+    project="train_table",
+    name="y26n_ball_player"
+)
